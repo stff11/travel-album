9923459d2c8bc2e279faae5392a51762ede79a44
StefanoFoffanoVS <stefano.foffano@visualsoft.co.uk>
2026-07-05 18:15:52 +0200

    new loggers

--- server/lib/logger.ts
+++ server/lib/logger.ts
@@ -1,6 +1,8 @@
 import pino from "pino";
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction =
+  process.env.NODE_ENV === "production" ||
+  process.env.NETLIFY === "true";
 
 export const logger = pino({
   level: process.env.LOG_LEVEL ?? "info",
@@ -18,3 +20,4 @@ export const logger = pino({
         },
       }),
 });
+
